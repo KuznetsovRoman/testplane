7e1b041951b088f98af5296712a387ceb58ee7d5
Roman Kuznetsov <kuznetsov.roman.orenburg@gmail.com>
2024-12-03 00:37:40 +0300

    test: check ubuntu firefox limits

--- e2e-test/testplane.config.ts
+++ e2e-test/testplane.config.ts
@@ -1,13 +1,5 @@
 import fs from "fs";
 
-const mkChrome = (browserVersion: string): Record<string, unknown> => ({
-    desiredCapabilities: {
-        browserName: "chrome",
-        browserVersion,
-        "goog:chromeOptions": { args: ["--no-sandbox"] },
-    },
-});
-
 const mkFirefox = (browserVersion: string): Record<string, unknown> => ({
     desiredCapabilities: {
         browserName: "firefox",
@@ -19,10 +11,30 @@ const osVersion = fs.readFileSync("/etc/os-release", "utf8");
 
 const ubuntuMilestone = Number((/VERSION_ID="(\d\d)/.exec(osVersion) as string[])[1]);
 
-const firefoxBrowsers = ["firefox133", "firefox130"];
+const firefoxBrowsers = [
+    "firefox133",
+    "firefox130",
+    "firefox129",
+    "firefox128",
+    "firefox127",
+    "firefox126",
+];
 
 if (ubuntuMilestone <= 22) {
-    firefoxBrowsers.push("firefox120", "firefox110", "firefox100");
+    firefoxBrowsers.push(
+        "firefox120",
+        "firefox110",
+        "firefox100",
+        "firefox99",
+        "firefox98",
+        "firefox97",
+        "firefox96",
+        "firefox95",
+        "firefox94",
+        "firefox93",
+        "firefox92",
+        "firefox91",
+    );
 }
 
 if (ubuntuMilestone <= 20) {
@@ -42,30 +54,32 @@ export default {
         parallelLimit: 1,
     },
     sets: {
-        chrome: {
-            files: ["testplane-tests/**/*.testplane.(t|j)s"],
-            browsers: ["chrome75", "chrome80", "chrome90", "chrome100", "chrome110", "chrome120", "chrome130"],
-        },
         firefox: {
             files: ["testplane-tests/**/*.testplane.(t|j)s"],
             browsers: firefoxBrowsers,
         },
     },
     browsers: {
-        chrome75: mkChrome("75.0"),
-        chrome80: mkChrome("80.0"),
-        chrome90: mkChrome("90.0"),
-        chrome100: mkChrome("100.0"),
-        chrome110: mkChrome("110.0"),
-        chrome120: mkChrome("120.0"),
-        chrome130: mkChrome("130.0"),
         firefox60: mkFirefox("60.0"),
         firefox70: mkFirefox("70.0"),
         firefox80: mkFirefox("80.0"),
         firefox90: mkFirefox("90.0"),
+        firefox91: mkFirefox("91.0"),
+        firefox92: mkFirefox("92.0"),
+        firefox93: mkFirefox("93.0"),
+        firefox94: mkFirefox("94.0"),
+        firefox95: mkFirefox("95.0"),
+        firefox96: mkFirefox("96.0"),
+        firefox97: mkFirefox("97.0"),
+        firefox98: mkFirefox("98.0"),
+        firefox99: mkFirefox("99.0"),
         firefox100: mkFirefox("100.0"),
         firefox110: mkFirefox("110.0"),
         firefox120: mkFirefox("120.0"),
+        firefox126: mkFirefox("126.0"),
+        firefox127: mkFirefox("127.0"),
+        firefox128: mkFirefox("128.0"),
+        firefox129: mkFirefox("129.0"),
         firefox130: mkFirefox("130.0"),
         firefox133: mkFirefox("133.0"),
     },
